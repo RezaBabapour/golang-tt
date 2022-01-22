374d3b1fc1a3f9f957d8fe1fcc6c5494607ee089
RezaBabapour <reza.babapour91@gmail.com>
2022-01-22 17:57:45 +0330

    prefork review

--- 06-fiber-tt/03-Prefork/main.go
+++ 06-fiber-tt/03-Prefork/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"log"
 )
 
 func main() {
-	app:=fiber.New(fiber.Config{Prefork: true})
+	//app:=fiber.New(fiber.Config{Prefork: true})
+	app:=fiber.New()
 	if !fiber.IsChild(){
 		fmt.Println("parent")
 	} else {
 		fmt.Println("child")
 	}
 	app.Get("/", func(ctx *fiber.Ctx) error {
+		log.Println("home page called")
 		return ctx.SendString("this is home")
 	})
 

b9bc80589b413d303c2181dcc9e14369632707f4
reza babapour <babapour@gap.im>
2022-02-06 20:44:11 +0330

    add slice

--- 08-Silce/main.go
+++ 08-Silce/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
 
 func main() {
 	s := make([]string, 5)
@@ -9,6 +13,8 @@ func main() {
 	fmt.Println(s)
 
 	s2d := make([][]int, 3)
+	fmt.Println("Type is : ", reflect.TypeOf(s2d))
+	fmt.Println("size : ", unsafe.Sizeof(s2d))
 	fmt.Println(s2d)
 	for i := 0; i < 3; i++ {
 		innerLen := i + 1
@@ -17,5 +23,6 @@ func main() {
 			s2d[i][j] = i + j
 		}
 	}
+	fmt.Println("size : ", unsafe.Sizeof(s2d))
 	fmt.Println(s2d)
 }

c83a45f3e976d15a2ee5420b449181d17cd8fd59
rezababapour <reza.babapour91@gmail.com>
2022-05-17 01:27:01 +0430

    add gitignore

--- 11-JsonWebResponse/main/Main.go
+++ 11-JsonWebResponse/main/Main.go
@@ -1,42 +1,38 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/gofiber/fiber/v2"
 	"io/ioutil"
-	"log"
-	"net/http"
 	"os"
 )
 
 type Server struct {
-	ServerAddr string `json:"serverAddr"`
+	ServerAddr []string `json:"servers"`
 }
 
 func main() {
-	jsonFile, err := os.Open("servers.json")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer jsonFile.Close()
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-	var result map[string]interface{}
-	json.Unmarshal([]byte(byteValue), &result)
-	strs := result["servers"].([]interface{})
-	servers := make([]interface{}, 0)
-	for _, i := range strs {
-		server := i.(map[string]interface{})
-		servers = append(servers, server["serverAddr"])
-	}
-	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		for _, server := range servers {
-			log.Println(r)
-			fmt.Fprintln(w, server.(string))
+
+	//var serverLinks Server
+	//json.Unmarshal([]byte(byteValue), &serverLinks)
+	//fmt.Printf("%+v", serverLinks)
+
+	app := fiber.New()
+	app.Get("/api", func(c *fiber.Ctx) error {
+		jsonFile, err := os.Open("servers.json")
+		if err != nil {
+			fmt.Println(err)
 		}
+		defer jsonFile.Close()
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+		//log.Println(c.Request())
+
+		headers := c.GetReqHeaders()
+		fmt.Println(headers["Deviceid"])
+
+		return c.Send(byteValue)
 	})
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	app.Listen(":8080")
 
 }
